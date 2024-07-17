e4ed297afd51c91e2497fb7d5bfe82e0a6ec8a1d
trannhatquy <quymyhungill@gmail.com>
2024-07-17 07:27:11 +0000

    Add llm choice

--- fingpt/app/entities/apis.py
+++ fingpt/app/entities/apis.py
@@ -3,11 +3,13 @@ from enum import Enum
 
 
 class LlmName(str, Enum):
+    Mistral = "mistral:7b-instruct-v0.3-fp16"
     GPT_4o = "gpt-4o"
+    Llama = "llama3:8b-instruct-fp16"
 
 
 class TickerReqDto(BaseModel):
     symbol: str
     sec_api_key: str
     llm_api_key: str
-    llm_name: LlmName = LlmName.GPT_4o
+    llm_name: LlmName
